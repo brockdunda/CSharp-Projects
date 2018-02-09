0ccea12a4d2e3513749d63bad057fbd884fd8991
Brock Dunda <brockdunda@gmail.com>
2018-02-09 15:39:12 -0800

    Adding new controller and views.

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -9,18 +9,33 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action.";
+        //}
+
+        // Update the index method to return a view
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action.";
+            return View();
         }
 
         // Get: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    // Given a parameter this returns the parameter based string.
+        //    //return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //    // Again given a parameter, update the html using the parameters. The ID parameter is contained as a segment in the route parameter.
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+
+        // Update the welcome method to include the parameters passed in to viewbags
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            // Given a parameter this returns the parameter based string.
-            //return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
-            // Again given a parameter, update the html using the parameters. The ID parameter is contained as a segment in the route parameter.
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
